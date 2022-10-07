174934775bd8275ce432ebf4d0fcdcd3486d12e5
Noor Muhammad <86077710+Noor-web@users.noreply.github.com>
2022-10-07 10:16:03 +0600

    ecom_site_07_Oct
    header and menu correction

---
+++ script.js
@@ -0,0 +1,40 @@
+// copy menu for mobile
+function copyMenu() {
+// copy inside .dpt-cat to departments
+var optCategory = document.querySelector('.dpt-cat');
+var optPlace = document.querySelector('.departments');
+optPlace.innerHTML = optCategory.innerHTML;
+
+// copy inside nav to nav
+var mainNav = document.querySelector('.header-nav nav');
+var navPlace = document.querySelector('.off-canvas nav');
+navPlace.innerHTML = mainNav.innerHTML;
+
+// copy .header-top .wrapper to theTopnav
+var topNav = document.querySelector('.header-top .wrapper');
+var topPlace = document.querySelector('.off-canvas .thetop-nav');
+topPlace.innerHTML = topNav.innerHTML;
+}
+copyMenu();
+
+//show mobile menu
+const menuButton = document.querySelector('.trigger');
+	closeButton = document.querySelector('.t-close');
+	addClass = document.querySelector('.site');
+menuButton.addEventListener('click', function(){
+	addClass.classList.toggle('showmenu')
+})
+closeButton.addEventListener('click', function(){
+	addClass.classList.remove('showmenu')
+})
+
+// show submenu on mobile
+const subMenu = document.querySelectorAll('.has-child .icon-small')
+subMenu.forEach((menu) => menu.addEventListener('click', toggle))
+
+function toggle(e) {
+	e.preventDefault();
+	subMenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('expand') : null)
+	if (this.closest('.has-child').classList != 'expand');
+	this.closest('.has-child').classList.toggle('expand')
+}
